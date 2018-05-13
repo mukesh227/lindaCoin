4da08e50966ea9eab91d12525a84b4aeb7572d4b
mukesh227 <mukeshiit@yahoo.com>
2018-05-13 14:32:21 +0530

    Update 106.cpp

--- 106.cpp
+++ 106.cpp
@@ -1,56 +1,6 @@
-// ////////////////////////////////////////////////////////
-// # Title
-// Special subset sums: meta-testing
-//
-// # URL
-// https://projecteuler.net/problem=106
-// http://euler.stephan-brumme.com/106/
-//
-// # Problem
-// Let `S(A)` represent the sum of elements in set `A` of size `n`.
-// We shall call it a special sum set if for any two non-empty disjoint subsets, `B` and `C`, the following properties are true:
-//
-// 1. `S(B) != S(C)`; that is, sums of subsets cannot be equal.
-// 2. If `B` contains more elements than `C` then `S(B) > S(C)`.
-//
-// For this problem we shall assume that a given set contains n strictly increasing elements and it already satisfies the second rule.
-//
-// Surprisingly, out of the 25 possible subset pairs that can be obtained from a set for which `n = 4`, only 1 of these pairs need to be tested for equality (first rule).
-// Similarly, when `n = 7`, only 70 out of the 966 subset pairs need to be tested.
-//
-// For `n = 12`, how many of the 261625 subset pairs that can be obtained need to be tested for equality?
-//
-// NOTE: This problem is related to Problem 103 and Problem 105.
-//
-// # Solved by
-// Stephan Brumme
-// August 2017
-//
-// # Algorithm
-// Only subsets of the same size are candidates, the remaining combinations are disallowed by rule 2.
-// I compare two subsets pair-wise, e.g. when looking at the subsets { 1,3 } and { 2,4 } I check whether 1<2 and 3<4.
-// If for each of those pair the first number is smaller than the second number then the sum of the first set must be smaller than the sum of the second set
-// and there is no need to check for equality (rule 1).
-// Therefore I count how often that test fails, i.e. how often I find a number in the first subset that is larger than its partner in the second subset.
-//
-// I wrote a brute-force solution quite some time ago. It solves the original problem in less than 0.01 seconds but times out for every Hackerrank test case.
-// Therefore I tried to find a better (= faster) algorithm. I read something about Catalan numbers ( https://en.wikipedia.org/wiki/Catalan_number )
-// and Dyck paths (explained in the same Wikipedia artible) - and had the correct result in less time.
-// However, my code was still definitely too slow for every Hackerrank test case.
-// My ''choose'' function from problem 116 needs severe improvement and I'm currently out of ideas ...
-//
-// Nevertheless, here is my brute force solution (which still finishes in less than 0.01 seconds) where I used a few tricks from my website http://bits.stephan-brumme.com .
-// The first step is to find all possible subsets:
-// - generate all `2^12` subsets as bitmasks: if the n-th bit of a bitmask is set, then the corresponding subset contains the n-th element
-// - store all those bitmasks/subsets where ''x'' bits are set in ''sets[x]''
-//
-// Then I compare each bitmask (''set1'') in ''sets[x]'' with all other bitmask (''set2'') in ''sets[x]'':
-// - repeatedly extract the lowest set bit: ''set1'' 's lowest bit must be always at a lower position than the lowest bit of ''set2''
-// - clear those lowest bits and continue until all bits are cleared
-//
-// # Hackerrank
-// As mentioned before, my program is way too slow for Hackerrank: you are supposed to find the result for sets with up to
-// `10^6` elements - and to make things worse, you have to do that for up to 30 sets within 2 seconds.
+//lindacoin blockchain code for termination of poverty and corruption
+//ln(O) algorithm for termination of 256 SHA proof of work
+
 
 #include <iostream>
 #include <vector>
